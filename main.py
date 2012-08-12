94de6f7f6e701ac4f3487e8bad2b49fd281a8964
jasonllinux <jasonllinux@gmail.com>
2012-08-12 20:00:25 +0800

    main

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
+# coding: utf-8
+
 import sys
 from PyQt4 import QtGui
+from PyQt4 import QtCore
+from PyQt4.QtGui import *  
+from PyQt4.QtCore import *
 from win import Ui_MainWindow
+import detect
 
-app = QtGui.QApplication(sys.argv)
+class Face(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        #add button funtion
+        QtCore.QObject.connect(self.ui.pushButton_2, QtCore.SIGNAL("clicked()"), self.file_open)
+        QtCore.QObject.connect(self.ui.pushButton_3, QtCore.SIGNAL("clicked()"), self.detect)
+    
+    def file_open(self):
+        self.pic_file = QFileDialog.getOpenFileName(self,"Open file dialog","/home/arch","Picture files(*.png *.jpg *.bmp)")
+        print self.pic_file
+        #filename ----->文件的整个路径
+        self.pic = QPixmap(self.pic_file)
+        self.pic.scaled(100, 100)
+        scene = QGraphicsScene()
+        scene.addPixmap(self.pic)
+        self.ui.graphicsView.setScene(scene)
+    
 
-mwin = QtGui.QMainWindow()
+    def detect(self):
+        print self.pic_file
+        detect.detectObject(str(self.pic_file))
+        #load to show after dected
+        self.pic = QPixmap(QString("face1.jpg"))
+        self.pic.scaled(100, 100)
+        scene = QGraphicsScene()
+        scene.addPixmap(self.pic)
+        self.ui.graphicsView.setScene(scene)
 
-ui = Ui_MainWindow()
 
-ui.setupUi(mwin)
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = Face()
+    myapp.show()
+    sys.exit(app.exec_())
 
-mwin.show()
-sys.exit(app.exec_())
